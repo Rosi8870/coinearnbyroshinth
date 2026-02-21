8a1471d077402660128b62758fc4df2193eb6be9
kanniyakumarione-test <kanniyakumarione@gmail.com>
2026-02-21 10:29:18 +0530

    argument changed

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
+const protect = require("../middleware/authMiddleware");
 const { buyUpgrade, getJackpot, buyTicket } = require("../controllers/gameController");
 
 router.post("/upgrade", protect, buyUpgrade);
